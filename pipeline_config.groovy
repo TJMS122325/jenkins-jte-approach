c81e82de0f0171d9fa8d3e959533c300a1c32788
jmiguel.externo <jmiguel.externo@cic.es>
2026-02-03 07:57:43 +0800

    fix pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -5,6 +5,11 @@ library_sources {
         branch = "main"
     }
 }
+    // also load libraries from the checked-out repository for local/in-repo testing
+    local {
+        path = "libraries"
+    }
+
 libraries{
     maven
 }

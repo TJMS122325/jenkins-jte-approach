8980376c6787f5d356cee7f273f457918713df22
jmiguel.externo <jmiguel.externo@cic.es>
2026-02-03 08:04:09 +0800

    fix config files

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,14 +1,3 @@
-library_sources {
-    github {
-        org = "TJMS122325"
-        repo = "jenkins-jte-approach"
-        branch = "main"
-    }
-}
-    // also load libraries from the checked-out repository for local/in-repo testing
-    local {
-        path = "libraries"
-    }
 
 libraries{
     maven
